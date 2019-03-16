228b284963452f89f78e390ddf5e381700c8ea12
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-03-16 17:04:06 +0000

    Update to newer APIs

--- BlazorOffsiteDemos.App/wwwroot/material-components/BlazorMaterialComponents.js
+++ BlazorOffsiteDemos.App/wwwroot/material-components/BlazorMaterialComponents.js
@@ -1,49 +1,67 @@
 window.BlazorMaterial = {
-  button: {
-    init: function (elem) {
-      mdc.ripple.MDCRipple.attachTo(elem);
-    }
-  },
+    button: {
+        init: function (elem) {
+            mdc.ripple.MDCRipple.attachTo(elem);
+        }
+    },
 
-  drawer: {
-    toggle: function (elem, isOpen) {
-      const drawer = mdc.drawer.MDCDrawer.attachTo(elem);
-      drawer.open = isOpen;
-    }
-  },
+    drawer: {
+        toggle: function (elem, isOpen) {
+            const drawer = mdc.drawer.MDCDrawer.attachTo(elem);
+            drawer.open = isOpen;
+        }
+    },
 
-  topAppBar: {
-    init: function (elem, scrollTarget) {
-      const topAppBar = mdc.topAppBar.MDCTopAppBar.attachTo(elem);
-      if (scrollTarget) {
-        topAppBar.setScrollTarget(document.querySelector(scrollTarget));
-      }
-    }
-  },
+    topAppBar: {
+        init: function (elem, scrollTarget) {
+            const topAppBar = mdc.topAppBar.MDCTopAppBar.attachTo(elem);
+            if (scrollTarget) {
+                topAppBar.setScrollTarget(document.querySelector(scrollTarget));
+            }
+        }
+    },
 
-  dialog: {
-    show: function (elem) {
-      const dialog = mdc.dialog.MDCDialog.attachTo(elem);
-      dialog.open();
-    }
-  },
+    dialog: {
+        show: function (elem) {
+            return new Promise(resolve => {
+                fixDialogActions(elem);
+                const dialog = mdc.dialog.MDCDialog.attachTo(elem);
+                const callback = event => {
+                    dialog.unlisten('MDCDialog:closing', callback);
+                    resolve(event.detail.action);
+                };
+                dialog.listen('MDCDialog:closing', callback);
+                dialog.open();
+            });
+        }
+    },
 
-  select: {
-    init: function (elem) {
-      mdc.select.MDCSelect.attachTo(elem);
-    }
-  },
-  
-  textField: {
-    init: function (elem) {
-      mdc.textField.MDCTextField.attachTo(elem);
-    }
-  },
+    select: {
+        init: function (elem) {
+            mdc.select.MDCSelect.attachTo(elem);
+        }
+    },
 
-  switch: {
-    setChecked: function (elem, isChecked) {
-      const switchControl = mdc.switchControl.MDCSwitch.attachTo(elem);
-      switchControl.checked = isChecked;
+    textField: {
+        init: function (elem) {
+            mdc.textField.MDCTextField.attachTo(elem);
+        }
+    },
+
+    switch: {
+        setChecked: function (elem, isChecked) {
+            const switchControl = mdc.switchControl.MDCSwitch.attachTo(elem);
+            switchControl.checked = isChecked;
+        }
     }
-  }
 };
+
+function fixDialogActions(elem) {
+    const tempAttributeName = 'temp-data-mdc-dialog-action';
+    const buttons = elem.querySelectorAll('[' + tempAttributeName + ']');
+    buttons.forEach(button => {
+        const dialogAction = button.getAttribute(tempAttributeName);
+        button.removeAttribute(tempAttributeName);
+        button.setAttribute('data-mdc-dialog-action', dialogAction);
+    });
+}
